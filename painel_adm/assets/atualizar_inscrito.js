2c52a4b3379dbee66549ac5b3302c633ef3de82e
rafammh <73232184+rafammh@users.noreply.github.com>
2023-03-06 08:28:18 -0300

    Update atualizar_inscrito.js

--- painel_adm/assets/atualizar_inscrito.js
+++ painel_adm/assets/atualizar_inscrito.js
@@ -1,14 +1,16 @@
-import { getUsuario } from "./usuario-get.js";
-import { updateUsuario } from "./usuario-upd.js";
+import { getParticipante } from "../../qwer/assets/js/participante-get.js";
+import { updateParticipante } from "../../qwer/assets/js/participante-upd.js";
 
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('id');
 
-const pais = sessionStorage.getItem('paislogado').replace(/"/g, "")
-const doc = sessionStorage.getItem('documentoLogado').replace(/"/g, "")
+const pais = id.substring(0, id.length - 10);
+const doc = id.substring(id.length - 10);
 
 console.log(doc);
 console.log(pais);
 console.log(id);
-getUsuario(id)
-updateUsuario(id)
+const nivel = 'adm'
+debugger
+getParticipante(doc, pais)
+updateParticipante(id, nivel)
